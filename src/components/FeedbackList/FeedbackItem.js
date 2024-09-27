c571f2e857c5b6fc902ce3817c12a30c3b834dac
Nhung <hongnhung16052003@gmail.com>
2024-09-27 22:30:21 +0700

    fix: icon star for the feedback section

--- src/components/FeedbackList/FeedbackItem.js
+++ src/components/FeedbackList/FeedbackItem.js
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { CheckedIcon, StarIcon } from '../Icon'
 import Image from '../Image'
 import styles from './Feedback.module.scss'
 import classNames from 'classnames/bind'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStarHalf } from '@fortawesome/free-regular-svg-icons'
 
 const cx = classNames.bind(styles)
 function FeedbackItem() {
@@ -14,31 +17,31 @@ function FeedbackItem() {
             <span className={cx('name')}>Hương Quỳnh</span>
             <div className={cx('star-list')}>
               <span className={cx('selected')}>
-                <StarIcon />
+                <FontAwesomeIcon icon={faStar} />
               </span>
               <span className={cx('selected')}>
-                <StarIcon />
+                <FontAwesomeIcon icon={faStar} />
               </span>
               <span className={cx('selected')}>
-                <StarIcon />
+                <FontAwesomeIcon icon={faStar} />
               </span>
-              <span>
-                <StarIcon />
+              <span className={cx('normal')}>
+                <FontAwesomeIcon icon={faStar} />
               </span>
-              <span>
-                <StarIcon />
+              <span className={cx('normal')}>
+                <FontAwesomeIcon icon={faStar} />
               </span>
             </div>
           </div>
         </div>
-              <p className={cx('content')}>Nhân viên nhà xe nhiệt tình lắm, 10đ không có gì để chê</p>
-              <div className={cx('bottom')}>
-                  <span className={cx('time')}>Đi ngày 07/09/2024</span>
-                  <p className={cx('status')}>
-                      <CheckedIcon width='1.8rem' height='1.8rem' />
-                      Đã mua vé
-                  </p>
-              </div>
+        <p className={cx('content')}>Nhân viên nhà xe nhiệt tình lắm, 10đ không có gì để chê</p>
+        <div className={cx('bottom')}>
+          <span className={cx('time')}>Đi ngày 07/09/2024</span>
+          <p className={cx('status')}>
+            <CheckedIcon width="1.8rem" height="1.8rem" />
+            Đã mua vé
+          </p>
+        </div>
       </div>
     </div>
   )
