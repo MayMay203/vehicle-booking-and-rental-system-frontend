3cdd53b1fad5a0cb65005c0f6865b9d6a40b7b98
Panda Nguyễn <ndbaochau104@gmail.com>
2024-09-27 20:45:49 +0700

    feat: build UI of rental page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,7 @@
 export const images = {
-    logo: require('./Safely Travel.png')
+  logo: require('./Safely Travel.png'),
+  slider1_rental_page: require('./slider1_rental_page.png'),
+  self_driving: require('./self_driving.png'),
+  manned: require('./manned.png'),
+  voucher: require('./voucher.png'),
 }
