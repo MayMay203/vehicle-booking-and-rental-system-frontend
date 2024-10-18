3298ac349107b13a2624cd4389ec0eb8cf4700b6
Nhung <hongnhung16052003@gmail.com>
2024-10-18 18:46:12 +0700

    reset menu back to user menu

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -6,47 +6,47 @@ import { config } from '~/config'
 
 const cx = classNames.bind(styles)
 function DefaultLayout({ children }) {
-  // const userMenu = [
-  //   {
-  //     content: 'Trang chủ',
-  //     to: config.routes.home,
-  //   },
-  //   {
-  //     content: 'Mua vé',
-  //     to: config.routes.ticket,
-  //   },
-  //   {
-  //     content: 'Thuê xe',
-  //     to: config.routes.renting,
-  //   },
-  //   {
-  //     content: 'Đặt xe',
-  //     to: config.routes.booking,
-  //   },
-  //   {
-  //     content: 'Trở thành đối tác',
-  //     to: config.routes.partner,
-  //   },
-  //   {
-  //     content: 'Hỏi đáp',
-  //     to: config.routes.answer,
-  //   },
-  //   {
-  //     content: 'Về chúng tôi',
-  //     to: config.routes.about,
-  //   },
-  // ]
-  const adminMenu = [
-    { content: 'Dịch vụ', to: config.routes.services },
-    { content: 'Tài khoản', to: config.routes.accounts },
-    { content: 'Đối tác', to: config.routes.partners },
-    { content: 'Thống kê', to: config.routes.statistics },
-    { content: 'Khuyến mãi', to: config.routes.vouchers },
+  const userMenu = [
+    {
+      content: 'Trang chủ',
+      to: config.routes.home,
+    },
+    {
+      content: 'Mua vé',
+      to: config.routes.ticket,
+    },
+    {
+      content: 'Thuê xe',
+      to: config.routes.renting,
+    },
+    {
+      content: 'Đặt xe',
+      to: config.routes.booking,
+    },
+    {
+      content: 'Trở thành đối tác',
+      to: config.routes.partner,
+    },
+    {
+      content: 'Hỏi đáp',
+      to: config.routes.answer,
+    },
+    {
+      content: 'Về chúng tôi',
+      to: config.routes.about,
+    },
   ]
+  // const adminMenu = [
+  //   { content: 'Dịch vụ', to: config.routes.services },
+  //   { content: 'Tài khoản', to: config.routes.accounts },
+  //   { content: 'Đối tác', to: config.routes.partners },
+  //   { content: 'Thống kê', to: config.routes.statistics },
+  //   { content: 'Khuyến mãi', to: config.routes.vouchers },
+  // ]
   // const partnerMenu = useMemo(() => ['Trang chủ', 'Dịch vụ', 'Đơn đặt', 'Thống kê', 'Thanh toán'], [])
   return (
     <div className={cx('wrapper')}>
-      <Header menus={adminMenu} />
+      <Header menus={userMenu} />
       <div className={cx('content')}>{children}</div>
       <Footer />
     </div>
--- src/layouts/HeaderOnly/HeaderOnly.js
+++ src/layouts/HeaderOnly/HeaderOnly.js
@@ -5,46 +5,46 @@ import { config } from '~/config'
 
 const cx = classNames.bind(styles)
 function HeaderOnly({ children }) {
-  //  const userMenu = [
-  //    {
-  //      content: 'Trang chủ',
-  //      to: config.routes.home,
-  //    },
-  //    {
-  //      content: 'Mua vé',
-  //      to: config.routes.ticket,
-  //    },
-  //    {
-  //      content: 'Thuê xe',
-  //      to: config.routes.renting,
-  //    },
-  //    {
-  //      content: 'Đặt xe',
-  //      to: config.routes.booking,
-  //    },
-  //    {
-  //      content: 'Trở thành đối tác',
-  //      to: config.routes.partner,
-  //    },
-  //    {
-  //      content: 'Hỏi đáp',
-  //      to: config.routes.answer,
-  //    },
-  //    {
-  //      content: 'Về chúng tôi',
-  //      to: config.routes.about,
-  //    },
-  //  ]
-   const adminMenu = [
-     { content: 'Dịch vụ', to: config.routes.services },
-     { content: 'Đối tác', to: config.routes.partners },
-     { content: 'Tài khoản', to: config.routes.accounts },
-     { content: 'Khuyến mãi', to: config.routes.vouchers },
-     { content: 'Thống kê', to: config.routes.statistics },
+   const userMenu = [
+     {
+       content: 'Trang chủ',
+       to: config.routes.home,
+     },
+     {
+       content: 'Mua vé',
+       to: config.routes.ticket,
+     },
+     {
+       content: 'Thuê xe',
+       to: config.routes.renting,
+     },
+     {
+       content: 'Đặt xe',
+       to: config.routes.booking,
+     },
+     {
+       content: 'Trở thành đối tác',
+       to: config.routes.partner,
+     },
+     {
+       content: 'Hỏi đáp',
+       to: config.routes.answer,
+     },
+     {
+       content: 'Về chúng tôi',
+       to: config.routes.about,
+     },
    ]
+  //  const adminMenu = [
+  //    { content: 'Dịch vụ', to: config.routes.services },
+  //    { content: 'Đối tác', to: config.routes.partners },
+  //    { content: 'Tài khoản', to: config.routes.accounts },
+  //    { content: 'Khuyến mãi', to: config.routes.vouchers },
+  //    { content: 'Thống kê', to: config.routes.statistics },
+  //  ]
   return (
     <div className={cx('wrapper')}>
-      <Header menus={adminMenu} />
+      <Header menus={userMenu} />
       <div className={cx('container')}>{children}</div>
     </div>
   )
