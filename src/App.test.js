6459e9103bba3c9682eb503fe2d071d3bf75f8e8
Nhung <hongnhung16052003@gmail.com>
2024-10-22 15:10:41 +0700

    fix: remove provider in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,15 @@
 import { render} from '@testing-library/react';
-import App from './App';
-import AuthModalProvider from './Context/AuthModalProvider'
-import UserProvider from './Context/UserProvider';
-import ServiceModalProvider from './Context/ServiceModalProvider'
-import GlobalModalProvider from './Context/GlobalModalProvider';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App'
+import UserProvider from './Context/UserProvider'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 test('renders learn react link', () => {
   render(
-   <GoogleOAuthProvider>
-      <GlobalModalProvider>
-        <UserProvider>
-          <AuthModalProvider>
-            <ServiceModalProvider>
-              <App />
-            </ServiceModalProvider>
-          </AuthModalProvider>
-        </UserProvider>,
-      </GlobalModalProvider>
-   </GoogleOAuthProvider>
+    <Provider store={store}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Provider>,
   )
 })
