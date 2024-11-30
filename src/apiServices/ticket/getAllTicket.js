689ead1ccb7fd52441f9f22e9708b841ec36aaf6
Nhung <hongnhung16052003@gmail.com>
2024-11-30 14:58:31 +0700

    fix: search ticket

--- src/apiServices/ticket/getAllTicket.js
+++ src/apiServices/ticket/getAllTicket.js
@@ -3,7 +3,7 @@ import * as httpRequest from '~/utils/httpRequest'
 export const getAllTickets = async (currentPage = 1, departureLocation, arrivalLocation, busName, departureDate, sortType) => {
   console.log(departureDate, departureLocation, arrivalLocation, busName)
   try {
-    let url = `/v1/user/busTripSchedules?departureDate=${departureDate}&size=${config.variables.pagesize}&page=${currentPage}&filter=busTrip.departureLocation: '${departureLocation}'`
+    let url = `/v1/user/busTripSchedules?departureDate=${departureDate}&size=${config.variables.pagesize}&page=${currentPage}&filter=busTrip.departureLocation: '${departureLocation}' and busTrip.arrivalLocation: '${arrivalLocation}'`
     if (busName !== 'all') {
       url += ` and busTrip.busPartner.businessPartner.businessName:'${busName}'`
     }
