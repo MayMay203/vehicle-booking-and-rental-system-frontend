e4665fb15d8d69db738b3a813f0ce3ab28eca84d
Nhung <hongnhung16052003@gmail.com>
2024-12-14 17:03:42 +0700

    handle functionality for updating message

--- src/apiServices/messageService/updateUnseenMessages.js
+++ src/apiServices/messageService/updateUnseenMessages.js
@@ -2,8 +2,8 @@ import { getAccessToken } from '~/utils/cookieUtils'
 import * as httpRequest from '~/utils/httpRequest'
 
 export const updateUnseenMessages = async (conversationId, idAccount, roleAccount) => {
-    console.log('Vô rồi nè!')
-    console.log(idAccount, roleAccount)
+  console.log('Vô rồi nè!')
+  console.log(idAccount, roleAccount)
   try {
     const response = await httpRequest.post(
       `/v1/chat/update-unseen-message?conversation_id=${conversationId}&account_id=${Number(idAccount)}&account_type=${roleAccount}`,
