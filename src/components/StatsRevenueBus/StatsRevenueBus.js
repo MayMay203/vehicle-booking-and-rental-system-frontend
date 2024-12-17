8f51930197d7b0bfbfa0acf866373cf95785c0b1
Nguyen Doan Bao Chau <ndbaochau104@gmail.com>
2024-12-17 22:18:23 +0700

    call api revenue bus partner

--- src/components/StatsRevenueBus/StatsRevenueBus.js
+++ src/components/StatsRevenueBus/StatsRevenueBus.js
@@ -1,143 +1,220 @@
-// import classNames from 'classnames/bind'
-// import styles from './StatsRevenueBus.module.scss'
-// import React, { useState } from 'react'
-// import { Line } from 'react-chartjs-2'
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { Col, Row } from 'react-bootstrap'
-// import Button from '../Button'
-// import { faCalendarDays, faSort } from '@fortawesome/free-solid-svg-icons'
-// import DatePicker from 'react-datepicker'
+// // import classNames from 'classnames/bind'
+// // import styles from './StatsRevenueBus.module.scss'
+// // import React, { useState } from 'react'
+// // import { Line } from 'react-chartjs-2'
+// // import {
+// //   Chart as ChartJS,
+// //   CategoryScale,
+// //   LinearScale,
+// //   PointElement,
+// //   LineElement,
+// //   Title,
+// //   Tooltip,
+// //   Legend,
+// // } from 'chart.js'
+// // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// // import { Col, Row } from 'react-bootstrap'
+// // import Button from '../Button'
+// // import { faCalendarDays, faSort } from '@fortawesome/free-solid-svg-icons'
+// // import DatePicker from 'react-datepicker'
+// import { Line } from '@ant-design/charts'
+// import React from 'react'
+// import { DatePicker } from 'antd'
+// import { statsRevenueBus } from '~/apiServices/busPartner/statsRevenueBus'
+// // import dayjs from 'dayjs'
+// // const cx = classNames.bind(styles)
+// // ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+// function StatsRevenueBus({ data }) {
+//   // const [startDate, setStartDate] = useState(new Date())
+//   // return (
+//   //   <div>
+
+//   //     <Row className="align-items-center">
+//   //       <Col className={cx('left-col')}>
+//   //         <div className={cx('background-red')}>
+//   //           <span className={cx('text')}>Từ: </span>
+//   //           <span className={cx('justify-content-center', 'content-calendar')}>
+//   //             {/* {startDate.toLocaleDateString('vi-VN')} */}
+//   //             {startDate.getFullYear()} {/* Chỉ hiển thị năm */}
+//   //           </span>
+//   //           <DatePicker
+//   //             selected={startDate}
+//   //             onChange={(date) => setStartDate(date)}
+//   //             dateFormat="yyyy"
+//   //             className={cx('content-calendar')}
+//   //             customInput={
+//   //               <FontAwesomeIcon icon={faCalendarDays} className={cx('justify-content-end', 'btn-calendar')} />
+//   //             }
+//   //             calendarClassName={cx('datetime-picker')}
+//   //             showYearPicker
+//   //           />
+//   //         </div>
+//   //       </Col>
+//   //       <Col className={cx('right-col')}>
+//   //         <Button rounded className={cx('btn-sort')}>
+//   //           <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+//   //           <span className={cx('d-none d-lg-inline')}>Sắp xếp</span>
+//   //         </Button>
+//   //       </Col>
+//   //     </Row>
+
+//   //     <Line
+//   //       data={{
+//   //         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+//   //         datasets: [
+//   //           {
+//   //             data: [2382, 2390, 2411, 2502, 2635, 2809, 2947, 3402, 3700, 4267, 4203, 4444],
+//   //             label: 'Doanh thu mỗi tháng',
+//   //             borderColor: '#FF7F50',
+//   //             fill: false,
+//   //           },
+//   //         ],
+//   //       }}
+//   //       options={{
+//   //         responsive: true,
+//   //         plugins: {
+//   //           title: {
+//   //             display: true,
+//   //             text: 'DOANH THU THEO NĂM',
+//   //             color: '#A33A3A',
+//   //             font: {
+//   //               size: 30,
+//   //             },
+//   //           },
+//   //           legend: {
+//   //             display: true,
+//   //             position: 'bottom',
+//   //             labels: {
+//   //               color: '#A33A3A',
+//   //               generateLabels: (chart) => {
+//   //                 const datasets = chart.data.datasets
+//   //                 return datasets.map((dataset, index) => ({
+//   //                   text: dataset.label,
+//   //                   fillStyle: dataset.backgroundColor || dataset.borderColor,
+//   //                   strokeStyle: dataset.borderColor,
+//   //                   lineWidth: 3,
+//   //                   hidden: !chart.isDatasetVisible(index),
+//   //                   datasetIndex: index,
+//   //                 }))
+//   //               },
+//   //             },
+//   //           },
+//   //         },
+//   //         scales: {
+//   //           x: {
+//   //             id: 'x',
+//   //             type: 'category',
+//   //             title: {
+//   //               display: true,
+//   //               text: 'Tháng',
+//   //               color: '#A33A3A',
+//   //               font: {
+//   //                 size: 16,
+//   //               },
+//   //             },
+//   //             ticks: {
+//   //               color: '#A33A3A',
+//   //             },
+//   //           },
+//   //           y: {
+//   //             id: 'y',
+//   //             type: 'linear',
+//   //             title: {
+//   //               display: true,
+//   //               text: 'Doanh thu (triệu đồng)',
+//   //               color: '#A33A3A',
+//   //               font: {
+//   //                 size: 16,
+//   //               },
+//   //             },
+//   //             ticks: {
+//   //               color: '#A33A3A',
+//   //             },
+//   //           },
+//   //         },
+//   //       }}
+//   //     />
+//   //   </div>
+//   // )
+//   const getRevenueBus = async() => {
+//     const respone = await statsRevenueBus('2024')
+//   }
+//   const rawData = {
+//     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+//     datasets: [
+//       {
+//         data: [2382, 2390, 2411, 2502, 2635, 2809, 2947, 3402, 3700, 4267, 4203, 4444],
+//         label: 'Doanh thu mỗi tháng',
+//         borderColor: '#FF7F50',
+//         fill: false,
+//       },
+//     ],
+//   }
+
+//   // Transform data for Ant Design Charts
+//   const formattedData = rawData.labels.map((month, index) => ({
+//     date: `Tháng ${month}`, // Format as 'Tháng 1', 'Tháng 2', etc.
+//     revenue: rawData.datasets[0].data[index], // Get the corresponding revenue
+//   }))
+
+//   const config = {
+//     data: formattedData,
+//     xField: 'date',
+//     yField: 'revenue',
+//     xAxis: {
+//       title: {
+//         text: 'Tháng',
+//         style: {
+//           fontSize: 14,
+//           fontWeight: 'bold',
+//         },
+//       },
+//     },
+//     yAxis: {
+//       title: {
+//         text: 'Doanh thu theo tháng(VND)',
+//       },
+//       label: {
+//         formatter: (value) => `${(value / 1000).toFixed(1)}K`, // Format as thousands
+//       },
+//     },
+//     color: ['#A33A3A'],
+//     smooth: true,
+//     point: {
+//       size: 5,
+//       shape: 'circle',
+//     },
+//   }
+//   const handleOnChange = () => {
+
+//   }
+//   return (
+//     <div>
+//       <DatePicker placeholder='Chọn năm' onChange={handleOnChange} picker="year" />
+//       <Line {...config} />
+//     </div>
+//   )
+// }
+// export default StatsRevenueBus
+
+import React, { useEffect, useState } from 'react'
+import { DatePicker } from 'antd'
 import { Line } from '@ant-design/charts'
-// import dayjs from 'dayjs'
-// const cx = classNames.bind(styles)
-// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
-function StatsRevenueBus({ data }) {
-  // const [startDate, setStartDate] = useState(new Date())
-  // return (
-  //   <div>
+import { Col } from 'react-bootstrap'
+import classNames from 'classnames/bind'
+import styles from './StatsRevenueBus.module.scss'
+import { statsRevenueBus } from '~/apiServices/busPartner/statsRevenueBus'
+const cx = classNames.bind(styles)
+function StatsRevenueBus() {
+  const [time, setTime] = useState('')
+  const [data, setData] = useState({ revenueStatistic: [] }) // Initialize with a safe default value
+  // const [revenueBy, setRevenueBy] = useState('')
 
-  //     <Row className="align-items-center">
-  //       <Col className={cx('left-col')}>
-  //         <div className={cx('background-red')}>
-  //           <span className={cx('text')}>Từ: </span>
-  //           <span className={cx('justify-content-center', 'content-calendar')}>
-  //             {/* {startDate.toLocaleDateString('vi-VN')} */}
-  //             {startDate.getFullYear()} {/* Chỉ hiển thị năm */}
-  //           </span>
-  //           <DatePicker
-  //             selected={startDate}
-  //             onChange={(date) => setStartDate(date)}
-  //             dateFormat="yyyy"
-  //             className={cx('content-calendar')}
-  //             customInput={
-  //               <FontAwesomeIcon icon={faCalendarDays} className={cx('justify-content-end', 'btn-calendar')} />
-  //             }
-  //             calendarClassName={cx('datetime-picker')}
-  //             showYearPicker
-  //           />
-  //         </div>
-  //       </Col>
-  //       <Col className={cx('right-col')}>
-  //         <Button rounded className={cx('btn-sort')}>
-  //           <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
-  //           <span className={cx('d-none d-lg-inline')}>Sắp xếp</span>
-  //         </Button>
-  //       </Col>
-  //     </Row>
-
-  //     <Line
-  //       data={{
-  //         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  //         datasets: [
-  //           {
-  //             data: [2382, 2390, 2411, 2502, 2635, 2809, 2947, 3402, 3700, 4267, 4203, 4444],
-  //             label: 'Doanh thu mỗi tháng',
-  //             borderColor: '#FF7F50',
-  //             fill: false,
-  //           },
-  //         ],
-  //       }}
-  //       options={{
-  //         responsive: true,
-  //         plugins: {
-  //           title: {
-  //             display: true,
-  //             text: 'DOANH THU THEO NĂM',
-  //             color: '#A33A3A',
-  //             font: {
-  //               size: 30,
-  //             },
-  //           },
-  //           legend: {
-  //             display: true,
-  //             position: 'bottom',
-  //             labels: {
-  //               color: '#A33A3A',
-  //               generateLabels: (chart) => {
-  //                 const datasets = chart.data.datasets
-  //                 return datasets.map((dataset, index) => ({
-  //                   text: dataset.label,
-  //                   fillStyle: dataset.backgroundColor || dataset.borderColor,
-  //                   strokeStyle: dataset.borderColor,
-  //                   lineWidth: 3,
-  //                   hidden: !chart.isDatasetVisible(index),
-  //                   datasetIndex: index,
-  //                 }))
-  //               },
-  //             },
-  //           },
-  //         },
-  //         scales: {
-  //           x: {
-  //             id: 'x',
-  //             type: 'category',
-  //             title: {
-  //               display: true,
-  //               text: 'Tháng',
-  //               color: '#A33A3A',
-  //               font: {
-  //                 size: 16,
-  //               },
-  //             },
-  //             ticks: {
-  //               color: '#A33A3A',
-  //             },
-  //           },
-  //           y: {
-  //             id: 'y',
-  //             type: 'linear',
-  //             title: {
-  //               display: true,
-  //               text: 'Doanh thu (triệu đồng)',
-  //               color: '#A33A3A',
-  //               font: {
-  //                 size: 16,
-  //               },
-  //             },
-  //             ticks: {
-  //               color: '#A33A3A',
-  //             },
-  //           },
-  //         },
-  //       }}
-  //     />
-  //   </div>
-  // )
   const rawData = {
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     datasets: [
       {
-        data: [2382, 2390, 2411, 2502, 2635, 2809, 2947, 3402, 3700, 4267, 4203, 4444],
+        data: data.revenueStatistic.map((item) => parseInt(item.revenue.replace('VND', '').trim(), 10)), // Safely access data
         label: 'Doanh thu mỗi tháng',
         borderColor: '#FF7F50',
         fill: false,
@@ -145,10 +222,9 @@ function StatsRevenueBus({ data }) {
     ],
   }
 
-  // Transform data for Ant Design Charts
   const formattedData = rawData.labels.map((month, index) => ({
-    date: `Tháng ${month}`, // Format as 'Tháng 1', 'Tháng 2', etc.
-    revenue: rawData.datasets[0].data[index], // Get the corresponding revenue
+    date: `Tháng ${month}`,
+    revenue: rawData.datasets[0].data[index] || 0, // Use 0 if data[index] is undefined
   }))
 
   const config = {
@@ -158,28 +234,73 @@ function StatsRevenueBus({ data }) {
     xAxis: {
       title: {
         text: 'Tháng',
-        style: {
-          fontSize: 14,
-          fontWeight: 'bold',
-        },
+        style: { fontSize: 14, fontWeight: 'bold' },
       },
     },
     yAxis: {
-      title: {
-        text: 'Doanh thu theo tháng(VND)',
-      },
+      title: { text: 'Doanh thu theo tháng (VND)' },
       label: {
-        formatter: (value) => `${(value / 1000).toFixed(1)}K`, // Format as thousands
+        formatter: (value) => `${(value / 1000).toFixed(1)}K`,
       },
     },
-    color: ['#409EFF'],
+    color: ['#A33A3A'],
     smooth: true,
     point: {
       size: 5,
       shape: 'circle',
+      style: {
+        fill: '#A33A3A',
+        stroke: '#A33A3A',
+      },
+    },
+    lineStyle: {
+      stroke: '#A33A3A',
+      lineWidth: 3,
     },
   }
 
-  return <Line {...config} />
+  const getRevenueBus = async (time) => {
+    try {
+      const response = await statsRevenueBus(time) // Fetch data
+      return response
+    } catch (error) {
+      console.error('Error fetching revenue data:', error)
+      return { revenueStatistic: [] } // Return fallback value on error
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (time !== '') {
+        const revenueData = await getRevenueBus(time)
+        setData(revenueData)
+      }
+    }
+    fetchData()
+  }, [time])
+
+  const handleOnChange = (date, dateString) => {
+    setTime(dateString)
+    console.log('Selected year:', dateString)
+  }
+
+  return (
+    <div className="mb-3 mt-3">
+      <div className="row mb-4">
+        <Col>
+          <DatePicker placeholder="Chọn năm" onChange={handleOnChange} picker="year" />
+        </Col>
+        <Col className={cx('justify-content-end', 'wrap-total')}>
+          <p className={cx('title-total')}>Tổng doanh thu</p>
+          <p className={cx('txt-total')}>{data?.totalRevenue}</p>
+        </Col>
+      </div>
+      <div style={{ flex: 1, marginLeft: '20px' }}>
+        <Line {...config} />
+      </div>
+    </div>
+  )
 }
+
 export default StatsRevenueBus
+
