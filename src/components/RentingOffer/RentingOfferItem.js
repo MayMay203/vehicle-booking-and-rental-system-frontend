c28626fed5009b9e17616d16555e4bb8bb080026
Nhung <hongnhung16052003@gmail.com>
2024-12-22 23:31:36 +0700

    call API for home page

--- src/components/RentingOffer/RentingOfferItem.js
+++ src/components/RentingOffer/RentingOfferItem.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from './RentingOffer.module.scss'
 import classNames from 'classnames/bind';
+import { config } from '~/config';
 
 const cx = classNames.bind(styles)
 function RentingOfferItem({title, src}) {
     return (
-      <Link className={cx('col-12','col-md-9','col-lg-4','item')}>
+      <Link className={cx('col-12','col-md-9','col-lg-4','item')} to={config.routes.renting}>
           <img className={cx('item-img')} alt="vehicle" src={src}></img>
         <div className={cx('title')}>{title}</div>
       </Link>
